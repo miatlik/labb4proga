f6673e4c080d36f6bee5e4808a8789f4c12cc3de
Yurkin PI-31 <jurkinmisha1@gmail.com>
2024-11-20 17:45:03 +0700

    Initial commit

---
+++ Card.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+class Card {
+    private int value; // Значение карты
+
+    public Card(int val) {
+        this.value = val;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
